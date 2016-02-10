6b6426c05e70ab19f902dfee27adbfaf2d9974f2
Antti Neijonen <aneijone@st-cn0001.oulu.fi>
2016-02-10 10:19:46 +0200

    Version 1 completed

--- test.py
+++ test.py
@@ -9,8 +9,28 @@ class TestSuite(unittest.TestCase):
     def test_one(self):
         app = FizzBuzz()
 
-
         self.failIf(app.calc(1) != 1)
+
+    def test_two(self):
+        app = FizzBuzz()
+        output = StringIO()
+        app.run(50, output)
+#        print output.getvalue().splitlines()[2]
+        self.failIf(output.getvalue().splitlines()[0] != "1")        
+        self.failIf(output.getvalue().splitlines()[1] != "2")     
+        self.failIf(output.getvalue().splitlines()[2] != "Fizz")
+        self.failIf(output.getvalue().splitlines()[4] != "Buzz")
+        self.failIf(output.getvalue().splitlines()[14] != "FizzBuzz")   
+        self.failIf(output.getvalue().splitlines()[44] != "FizzBuzz")   
+
+    def test_three(self):
+        app = FizzBuzz()
+        output = StringIO()
+        app.run(100, output)
+        
+        self.failIf(output.getvalue().splitlines().count("Fizz") != 27)
+        self.failIf(output.getvalue().splitlines().count("Buzz") != 14)
+        self.failIf(output.getvalue().splitlines().count("FizzBuzz") != 6)
 
     def test_run(self):
         output = StringIO()
